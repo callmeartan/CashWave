d3e673bfe27360c96922259f8a00fc503ccd89a4
callmeartan <artanahmadi@icloud.com>
2024-04-24 20:41:42 +0300

    Initial Commit

---
+++ CashWave/CashWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashWaveApp.swift
+//  CashWave
+//
+//  Created by Artan Ahmadi on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CashWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
